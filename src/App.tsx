6756138e674716e6fdc07080d99b861e0db96f84
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-23 22:18:22 -0300

    ft

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 
-import './App.css'
-import Header from './Componentes/Header'
-import Principal from './Componentes/Principal/principal'
-import Footer from './Componentes/Footerr/Footer'
+import './App.css';
+import Footer from './Componentes/Footerr/Footer';
+import Header from './Componentes/Header';
+import Principal from './Componentes/Principal/principal';
+import Cleitao from './assets/tela.png';
 
 function App() {
-  
+
 
   return (
     <>
-    <Header />
-    
-    <article>    
-    <h1 className='fonte'>GOD OF WAR: RAGNAROK</h1>
-    <div>
-      <p className='fonte1'>AVALIAÇÃO DA RATINGS</p>
-      <div className='alinhar'> 
-      <img height={24} src="../src/assets/image5.png" alt="" />
-      <p>9,6/10</p>
-      </div>
-    </div>
-    <div>
-      <p className='fonte1'>SUA AVALIAÇÃO</p>
+      <Header />
 
-      <div className='alinhar'>  
-      <img height={24} src="../src/assets/image.png" alt="" />
-      <p>Avaliar</p>
-      </div>
-    </div>
-    </article>
+      <article>
+        <h1 className='fonte'>GOD OF WAR: RAGNAROK</h1>
+        <div>
+          <p className='fonte1'>AVALIAÇÃO DA RATINGS</p>
+          <div className='alinhar'>
+            <img height={24} src="../src/assets/image5.png" alt="" />
+            <p>9,6/10</p>
+          </div>
+        </div>
+        <div>
+          <p className='fonte1'>SUA AVALIAÇÃO</p>
 
-    <figure>
-      <img height="400px" width="400px" src="../src/assets/tela.png" alt="" />
-      <img height="400px" width="600px" src="../src/assets/image4.png" alt="" />
-    </figure>
-    <Principal/>
-    <Footer/>
+          <div className='alinhar'>
+            <img height={24} src="../src/assets/image.png" alt="" />
+            <p>Avaliar</p>
+          </div>
+        </div>
+      </article>
+
+      <figure>
+        <img height="400px" width="400px" src={Cleitao} alt="" />
+        <img height="400px" width="600px" src="../src/assets/image4.png" alt="" />
+      </figure>
+      <Principal />
+      <Footer />
     </>
   )
 }
